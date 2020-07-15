ebd5c506ca85a3d20ef98a0ad8a3068b4e66f602
Eric C <erix2205@gmail.com>
2020-07-15 10:21:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function takeANumber(katzDeliLine , name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.length > 0){
-    return `Currently serving ${katzDeliLine[0]}.`
     katzDeliLine.shift(`${katzDeliLine[0]}`);
+    return `Currently serving ${katzDeliLine[0]}.`
   }
   else {
     return 'There is nodoby waiting to be served!'
