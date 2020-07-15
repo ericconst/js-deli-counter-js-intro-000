b5a884850e715403ca3975533eba68649129954d
Eric C <erix2205@gmail.com>
2020-07-15 10:07:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(katzDeliLine , name){
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.lentgh === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    console.log(`${katzDeliLine[0]}`);
+  }
+  katzDeliLine.shift(katzDeliLine[0])
+}
