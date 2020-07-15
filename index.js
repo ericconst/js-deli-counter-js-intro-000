13bf1d512367925a40931c5f97563f7ee8072e9d
Eric C <erix2205@gmail.com>
2020-07-15 10:12:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function takeANumber(katzDeliLine , name){
 
 function nowServing(katzDeliLine){
   if (katzDeliLine.lentgh === 0){
-    console.log(There is nobody waiting to be served!);
+    console.log(`There is nobody waiting to be served!`);
   } else {
     console.log(`Currently serving ${katzDeliLine[0]}`);
   }
